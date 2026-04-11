35b77643ddd6874d7cd165b62387bfad027c29fe
Sekou <sekoudiane1990@gmail.com>
2026-04-11 03:56:26 +0300

    Create main4.py

---
+++ PISMR/pr7/main4.py
@@ -0,0 +1,162 @@
+#NEW 2 - поправил направления слайсов, граничные индексы, имена перменных вложенных циклов, обрезку ветвей "ok1 and"
+# в ф-ции check_upper_bound_for_current_path
+# в ф-ции estimate_lower_bound_for_remaining_path
+# строка 100 дописал "...ok1 and..."
+
+import sys, pygame, numpy as np, math
+import itertools
+
+DEBUG = False
+
+pygame.font.init()
+
+def draw_text(screen, s, x, y, sz=18, c=(0, 0, 0)):  # отрисовка текста
+    screen.blit(pygame.font.SysFont('Comic Sans MS', sz).render(s, True, c), (x, y))
+
+def dist(p1, p2): return np.linalg.norm(np.subtract(p1, p2))  # расстояние между точками
+
+def rot(v, ang): return np.dot([[-v[1], v[0]], v], [math.sin(ang), math.cos(ang)])  # поворот вектора на угол
+
+def rot_arr(vv, ang): return [rot(v, ang) for v in vv]  # функция для поворота массива на угол
+
+def lim_ang(ang, arc=3.141592653589793):  # ограничение угла в пределах +/-pi
+    ang = ang % (2 * arc)
+    return ang + (2 * arc if ang < -arc else -2 * arc if ang > arc else 0)
+
+def greedy_tsp(pts, ind):  # жадное разомкнутое решение задачи коммивояжера (поиск в глубину)
+    buf, res = [np.array(pts[i]) for i in range(len(pts)) if i != ind], [np.array(pts[ind])]
+    while len(buf): res += [buf.pop(np.argmin([np.hypot(*(res[-1] - p)) for p in buf]))]
+    return res
+
+def full_searсh(pts, ind):  # полный перебор возможных маршрутов
+    min_len, min_path = np.inf, []  # NEW
+    ii = [i for i in range(len(pts)) if i != ind]
+    perms = itertools.permutations(ii)
+    print(f"Num permutations: {math.factorial(len(ii))}")
+    for step, perm in enumerate(perms):
+        path = [pts[ind]] + [pts[i] for i in perm]
+        L = calc_path_len(path)
+        if L < min_len: min_len, min_path = L, path
+        if step % 1000 == 0: print(f"Step: {step + 1}")
+    return min_path
+
+def branch_and_bound_searсh(pts, ind):  # применение метода ветвей и границ
+    min_len, min_path = np.inf, greedy_tsp(pts, ind)
+    L_upper = calc_path_len(min_path)
+
+    ii = [i for i in range(len(pts)) if i != ind]
+    perms = itertools.permutations(ii)
+    print(f"Num permutations: {math.factorial(len(ii))}")
+
+    # проверка удовлетворительности решения (непревышения жадного пути или ранее найденного минимального пути)
+    def check_upper_bound_for_current_path(ind, perm, L_upper, pts):
+        s = dist(pts[ind], pts[perm[0]]) # NEW2
+        if DEBUG: print(f"Step {-1}: p1={pts[ind]}, p2 = {pts[perm[0]]}: s={s}")
+        for i in range(len(perm) - 1):
+            s += dist(pts[perm[i]], pts[perm[i + 1]]) # NEW2
+            if DEBUG: print(f"Step {i}: p1={pts[perm[i]]}, p2 = {pts[perm[i + 1]]}: s={s}")
+            visited_pts = [pts[ind]] + [pts[j] for j in perm[:i + 2]]  # NEW2 i + 2, NEW2 j!=i
+            L_lower = s + estimate_lower_bound_for_remaining_path(visited_pts, pts)
+            if L_lower > L_upper: return False, perm[:i + 2] # NEW2 i + 2
+        return True, None
+
+    def estimate_lower_bound_for_remaining_path(visited_pts, pts):  # уточняем нижнюю оценку оставшегося маршрута
+        targets = visited_pts[-1:] + [p for p in pts if not p in visited_pts]
+        if DEBUG: print(f"targets = {targets}")
+        buf = [*targets[1:]]  # в буфере первых точек: последняя из пройденного маршрута и все непройденные
+        res = 0
+        while len(buf):
+            dd = [dist(buf[0], p) for p in targets]
+            dd[np.argmin(dd)] = np.inf  # маскируем нулевое расстояние
+            ind = np.argmin(dd)
+            if DEBUG: print(f"+ {dd[ind]} //{buf[0]}, {targets[ind]}")
+            res += dd[ind]
+            buf.pop(0)
+        if DEBUG: print(f"Lower_bound = {res}")
+        return res
+
+    bad_trunks = []  # плохие крупные ветви, на которых "отсекаем" мелкие
+
+    def check_deletion(perm, bad_trunks):  # проверка, не является ли ветвь удаленной
+        for bt in bad_trunks:
+            res = True
+            for i in range(len(bt)):
+                if bt[i] != perm[i]: res = False
+            if res: return True
+        return False
+
+    # [132, 4561, 23, ...]
+    for step, perm in enumerate(perms):  # [2345]
+        print(f"Step: {step + 1}")
+        ok1 = not check_deletion(perm, bad_trunks)
+        if not ok1:  # ветвь уже запрещена - уходим на след. итерацию
+            path = [pts[ind]] + [pts[i] for i in perm]
+            L = calc_path_len(path)
+            if DEBUG: print(f"Skipping deleted branch: ind+perm={ind}+{perm}, path={path}, L={L}")
+            else: print("Skipping deleted branch")
+            continue
+        ok2, trunk = check_upper_bound_for_current_path(ind, perm, L_upper, pts)  # NEW
+
+        # ветвь не запрещена, но верхняя граница не соблюдается - добавляем запрет и уходим на след. итерацию
+        if ok1 and not ok2:  #NEW2 "ok1 and"
+            bad_trunks.append(trunk)
+            # TODO: ДОБАВИТЬ УДАЛЕНИЕ БОЛЕЕ ЧАСТНЫХ ВЕТВЕЙ - например (0, 8, 2) после добавления (0, 8)
+            if DEBUG: print(f"Added deleted branch: perm={perm}")
+            else: print("Added deleted branch")
+            continue
+
+        path = [pts[ind]] + [pts[i] for i in perm]
+        L = calc_path_len(path) #РЕСУРСОЕМКАЯ ОПЕРАЦИЯ, КОТОРУЮ СТРЕМИМЧЯ ПРОПУСКАТЬ
+
+        if L < min_len:
+            min_len, min_path, L_upper = L, path, L
+            print("Found better path")
+
+    print("BAD TRUNKS: ", bad_trunks)
+    return min_path
+
+sz = (800, 600)
+
+def calc_path_len(path):
+    res = 0
+    for i in range(len(path) - 1): res += dist(path[i], path[i + 1])
+    return res
+
+if __name__ == "__main__":
+    screen, timer, fps = pygame.display.set_mode(sz), pygame.time.Clock(), 20
+    pygame.display.set_caption('Animation 2D')
+    dt = 1 / fps
+
+    pts = [(100, 100), (150, 200), (250, 200),
+           (250, 100), (150, 150), (350, 100), (150, 300),
+           (200, 150), (250, 300), (100, 200)]
+
+    START_IND = 4
+    path = greedy_tsp(pts, START_IND)
+
+    while True:
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT: sys.exit(0)
+            if ev.type == pygame.KEYDOWN:
+                if ev.key == pygame.K_1:
+                    path = full_searсh(pts, START_IND)
+                    L = calc_path_len(path)
+                if ev.key == pygame.K_2:
+                    path = branch_and_bound_searсh(pts, START_IND)
+                    L = calc_path_len(path)
+                    print(trunks)
+
+        screen.fill((255, 255, 255))
+
+        for i in range(len(path) - 1):
+            pygame.draw.line(screen, (255, 0, 0), path[i], path[i + 1], 2)
+
+        for i, p in enumerate(pts):
+            pygame.draw.circle(screen, (0, 0, 0), p, 3, 0)
+            draw_text(screen, f"{i}", *p)
+
+        L = calc_path_len(path)
+        draw_text(screen, f"L = {L}", 5, 5)
+        pygame.display.flip(), timer.tick(fps)
+
+# template file by S. Diane, RTU MIREA, 2024-2026
